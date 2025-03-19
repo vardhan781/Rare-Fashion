3004dc8aa52812c50a59b4a6b802ba2d2042fdd7
vardhan781 <vardhansinhmandora@gmail.com>
2025-03-19 15:36:13 +0530

    Related Products Logic

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -7,14 +7,24 @@ const RelatedProducts = ({ category }) => {
   const { products } = useContext(ShopContext);
   const [related, setRelated] = useState([]);
 
+  const shuffleArray = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
+
   useEffect(() => {
     if (products.length > 0) {
-      let productsCopy = products.slice();
-      productsCopy = productsCopy.filter((item) => category === item.category);
+      let productsCopy = products.filter((item) => category === item.category);
 
-      setRelated(productsCopy.slice(0, 4));
+      const shuffledProducts = shuffleArray(productsCopy);
+
+      setRelated(shuffledProducts.slice(0, 4));
     }
-  }, [products]);
+  }, [products, category]);
   return (
     <div className="display-related">
       <h1>You Might Also Like</h1>
